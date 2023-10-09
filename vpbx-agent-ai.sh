919dde6fa38480033570ba8771ef5f5a851058b9
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2023-10-09 08:26:31 -0600

    Update vpbx-agent-ai.sh

--- vpbx-agent-ai.sh
+++ vpbx-agent-ai.sh
@@ -10,12 +10,12 @@ mkdir /var/lib/asterisk/agi-bin/docs
 mkdir /var/lib/asterisk/agi-bin/data
 wget https://raw.githubusercontent.com/VitalPBX/vitalpbx_agent_ai_chatgpt/main/chatbot.py -P /var/lib/asterisk/agi-bin/
 wget https://raw.githubusercontent.com/VitalPBX/vitalpbx_agent_ai_chatgpt/main/record-prompt.py -P /var/lib/asterisk/agi-bin/
-wget https://raw.githubusercontent.com/VitalPBX/vitalpbx_agent_ai_chatgpt/main/vpbx-agent-ai-embeded.py -P /var/lib/asterisk/agi-bin/
+wget https://raw.githubusercontent.com/VitalPBX/vitalpbx_agent_ai_chatgpt/main/vpbx-agent-ai-embedded.py -P /var/lib/asterisk/agi-bin/
 wget https://raw.githubusercontent.com/VitalPBX/vitalpbx_agent_ai_chatgpt/main/vpbx-agent-ai.py -P /var/lib/asterisk/agi-bin/
-wget https://raw.githubusercontent.com/VitalPBX/vitalpbx_agent_ai_chatgpt/main/vpbx-embeded-docs.py -P /var/lib/asterisk/agi-bin/
+wget https://raw.githubusercontent.com/VitalPBX/vitalpbx_agent_ai_chatgpt/main/vpbx-embedded-docs.py -P /var/lib/asterisk/agi-bin/
 chmod +x /var/lib/asterisk/agi-bin/chatbot.py
 chmod +x /var/lib/asterisk/agi-bin/record-prompt.py
-chmod +x /var/lib/asterisk/agi-bin/vpbx-agent-ai-embeded.py
+chmod +x /var/lib/asterisk/agi-bin/vpbx-agent-ai-embedded.py
 chmod +x /var/lib/asterisk/agi-bin/vpbx-agent-ai.py
-chmod +x /var/lib/asterisk/agi-bin/vpbx-embeded-docs.py
+chmod +x /var/lib/asterisk/agi-bin/vpbx-embedded-docs.py
 wget https://raw.githubusercontent.com/VitalPBX/vitalpbx_agent_ai_chatgpt/main/extensions__70-agent-ai.conf -P /etc/asterisk/vitalpbx/
