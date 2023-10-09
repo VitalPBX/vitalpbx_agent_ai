f7f154c54b127b9f1f3b143e60bbb5d27aa118e3
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2023-10-09 05:52:49 -0600

    Update vpbx-agent-ai.sh

--- vpbx-agent-ai.sh
+++ vpbx-agent-ai.sh
@@ -8,14 +8,14 @@ set -e
 cd /var/lib/asterisk/agi-bin/
 mkdir /var/lib/asterisk/agi-bin/docs
 mkdir /var/lib/asterisk/agi-bin/data
-wget https://raw.githubusercontent.com/VitalPBX/vitalpbx_agent_ai_chatgpt/main/chatbot.py /var/lib/asterisk/agi-bin/
-wget https://raw.githubusercontent.com/VitalPBX/vitalpbx_agent_ai_chatgpt/main/record-prompt.py /var/lib/asterisk/agi-bin/
-wget https://raw.githubusercontent.com/VitalPBX/vitalpbx_agent_ai_chatgpt/main/vpbx-agent-ai-embeded.py /var/lib/asterisk/agi-bin/
-wget https://raw.githubusercontent.com/VitalPBX/vitalpbx_agent_ai_chatgpt/main/vpbx-agent-ai.py /var/lib/asterisk/agi-bin/
-wget https://raw.githubusercontent.com/VitalPBX/vitalpbx_agent_ai_chatgpt/main/vpbx-embeded-docs.py /var/lib/asterisk/agi-bin/
+wget https://raw.githubusercontent.com/VitalPBX/vitalpbx_agent_ai_chatgpt/main/chatbot.py -P /var/lib/asterisk/agi-bin/
+wget https://raw.githubusercontent.com/VitalPBX/vitalpbx_agent_ai_chatgpt/main/record-prompt.py -P /var/lib/asterisk/agi-bin/
+wget https://raw.githubusercontent.com/VitalPBX/vitalpbx_agent_ai_chatgpt/main/vpbx-agent-ai-embeded.py -P /var/lib/asterisk/agi-bin/
+wget https://raw.githubusercontent.com/VitalPBX/vitalpbx_agent_ai_chatgpt/main/vpbx-agent-ai.py -P /var/lib/asterisk/agi-bin/
+wget https://raw.githubusercontent.com/VitalPBX/vitalpbx_agent_ai_chatgpt/main/vpbx-embeded-docs.py -P /var/lib/asterisk/agi-bin/
 chmod +x /var/lib/asterisk/agi-bin/chatbot.py
 chmod +x /var/lib/asterisk/agi-bin/record-prompt.py
 chmod +x /var/lib/asterisk/agi-bin/vpbx-agent-ai-embeded.py
 chmod +x /var/lib/asterisk/agi-bin/vpbx-agent-ai.py
 chmod +x /var/lib/asterisk/agi-bin/vpbx-embeded-docs.py
-wget https://raw.githubusercontent.com/VitalPBX/vitalpbx_agent_ai_chatgpt/main/extensions__70-agent-ai.conf /etc/asterisk/vitalpbx/
+wget https://raw.githubusercontent.com/VitalPBX/vitalpbx_agent_ai_chatgpt/main/extensions__70-agent-ai.conf -P /etc/asterisk/vitalpbx/
