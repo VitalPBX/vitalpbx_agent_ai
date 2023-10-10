5f4d9d578af94e7200a6c72cb0d2fad7c1f47785
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2023-10-10 07:27:16 -0600

    Update vpbx-agent-ai.sh

--- vpbx-agent-ai.sh
+++ vpbx-agent-ai.sh
@@ -5,6 +5,9 @@
 # VitalPBX Agent AI with ChatGPT(Embedded), Whisper and Azure Speech(TTS)
 #
 set -e
+echo -e "************************************************************"
+echo -e "*          Welcome to the AI Agent installation            *"
+echo -e "************************************************************"
 cd /var/lib/asterisk/agi-bin/
 mkdir /var/lib/asterisk/agi-bin/docs
 mkdir /var/lib/asterisk/agi-bin/data
@@ -19,3 +22,16 @@ chmod +x /var/lib/asterisk/agi-bin/vpbx-agent-ai-embedded.py
 chmod +x /var/lib/asterisk/agi-bin/vpbx-agent-ai.py
 chmod +x /var/lib/asterisk/agi-bin/vpbx-embedded-docs.py
 wget https://raw.githubusercontent.com/VitalPBX/vitalpbx_agent_ai_chatgpt/main/extensions__70-agent-ai.conf -P /etc/asterisk/vitalpbx/
+asterisk -rx"dialplan reload"
+echo -e "\n"
+echo -e "************************************************************"
+echo -e "*       All components have been installed correctly       *"
+echo -e "*             To make inquiries with ChatGPT               *"                  
+echo -e "*        Dial *778 for English or *779 for Spanish         *"
+echo -e "*     For queries obtained from custom documentation       *"
+echo -e "*    First upload the PDF document in the docs folder      *"
+echo -e "*         Then run from the console the command:           *"
+echo -e "*                ./vpbx-embedded-docs.py                   *"
+echo -e "*         To make inquiries with ChatGPT-Embedded          *"                  
+echo -e "*        Dial *888 for English or *889 for Spanish         *"
+echo -e "************************************************************"
