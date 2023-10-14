a965276cb6914a2b008e5ec6e5ebbef03f3ef921
rodrigocuadra <30705964+rodrigocuadra@users.noreply.github.com>
2023-10-14 07:45:20 -0600

    Update chatserver.py

--- chatserver.py
+++ chatserver.py
@@ -1,13 +1,14 @@
 # chatserver.py
 import asyncio
 import websockets
-# Uncomment if you are using a valid domain with ssl
-#import  ssl
+# Uncomment if you are using a valid domain with SSL
+#import ssl
 #import logging
 
+# Create a set to store connected WebSocket clients
 connected = set()
 
-# Uncomment if you are using a valid domain with ssl
+# Uncomment if you are using a valid domain with SSL
 # logging.basicConfig()
 
 #ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
@@ -16,26 +17,28 @@ connected = set()
 #ssl_key = "/usr/share/vitalpbx/certificates/your.domain/private.pem"
 
 async def echo(websocket, path):
-    # Register.
+    # Register the WebSocket client.
     print(f"Client {websocket.remote_address} connected.")
     connected.add(websocket)
 
     try:
         async for message in websocket:
-            # Message received from client
+            # Message received from the client
             print(f"Received message from {websocket.remote_address}: {message}")
 
+            # Broadcast the message to all connected clients except the sender
             for conn in connected:
                 if conn != websocket:
                     await conn.send(message)
     finally:
-        # Unregister.
+        # Unregister the WebSocket client when they disconnect.
         connected.remove(websocket)
         print(f"Client {websocket.remote_address} disconnected.")
 
-# If you are not going to use SSL just enter the IP of your server, otherwise enter the domain.
+# If you are not using SSL, enter the IP of your server; otherwise, enter the domain.
 start_server = websockets.serve(echo, 'IP or Domain', 3001)
 print("WebSocket server started on ws://IP or Domain:3001")
 
+# Start the WebSocket server
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
